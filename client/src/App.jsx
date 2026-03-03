21681cf189a0acc1588bd3ff793a50a9e39ded47
Pushkar <rawatprince815@gmail.com>
2026-03-03 22:43:49 +0530

    signin page ready...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login.jsx'
+import Feed from './pages/Feed.jsx'
+import Messages from './pages/Messages.jsx'
+import ChatBot from './pages/ChatBot.jsx'
+import Connections from './pages/Connections.jsx'
+import Discover from './pages/Discover.jsx'
+import Profile from './pages/Profile.jsx'
+import CreatePost from './pages/CreatePost.jsx'
 
 const App = () => {
   return (
-    <div></div>
+    <>
+      <Routes>
+        <Route path='/' element={<Login />}>
+          <Route index element={<Feed />} />
+          <Route path='messages' element={<Messages />} />
+          <Route path='messages/:userId' element={<ChatBot />} />
+          <Route path='connections' element={<Connections />} />
+          <Route path='discover' element={<Discover />} />
+          <Route path='profile' element={<Profile />} />
+          <Route path='profile/:profileId' element={<Profile />} />
+          <Route path='create-post' element={<CreatePost />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
